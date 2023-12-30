e53e2b54724e2be8e44ddb0655749864afcb3d25
Tarun Hawdia <tarunhawdiya112@gmail.com>
2023-12-30 11:12:23 +0530

    add todo react commmit

--- todo-frontend/src/App.jsx
+++ todo-frontend/src/App.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import './App.css'
+import { useState } from 'react'
 
-function App() {
+
+function App(){
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+
+  const addTodo = () => {
+    if(todo!==''){
+      setTodos([...todos , todo]);
+      setTodo('');
+  }
+  };
+
 
   return (
-    <div>
-      hello world!
-    </div>
+    <>
+      <h1>TODOS</h1>
+      <div className='input-container'>
+
+      <input type="text" placeholder="Enter a task" value={todo} onChange={(e)=>{
+        setTodo(e.target.value)
+      }}></input>
+      <button className='add-todos' onClick={addTodo}> + Add Todos</button>
+      </div>
+    </>
   )
 }
 
